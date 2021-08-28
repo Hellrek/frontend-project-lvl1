e7bc0451e5a7a9c5c5ca35717c60721899c43e56
Kerik <hellrz@bk.ru>
2021-08-28 15:56:05 +0300

    finat of stap 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,13 +1,8 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
+// eslint-disable-next-line import/extensions
 import usrName from '../src/cli.js';
 
-// console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!');
 
 console.log(`Hello, ${usrName}!`);
-
-/*
-$ brain-games
-Welcome to the Brain Games!
-May I have your name? John
-Hello, John!
-*/
--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,3 @@
 import readlineSync from 'readline-sync';
 
-const youName = readlineSync.question('May I have your name?:');
-export default youName;
+export default readlineSync.question('May I have your name?:');
